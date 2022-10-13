64113f2289f83b800cb036f09dd5a9dca270172c
Giovanna de Souza Almeida <gi.souza03@gmail.com>
2022-10-13 18:59:47 -0300

    salvando imoveis nivel basico

---
+++ src/main/java/com/example/desafio02/controller/ImovelController.java
@@ -0,0 +1,27 @@
+package com.example.desafio02.controller;
+
+import com.example.desafio02.model.Imovel;
+import com.example.desafio02.service.IImovel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/imoveis")
+public class ImovelController {
+    @Autowired
+    private IImovel imovelService;
+
+    @PostMapping("/salvar")
+    public ResponseEntity<Imovel> salvarImovel(@RequestBody Imovel imovel){
+        return new ResponseEntity<>(imovelService.salvarImovel(imovel), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Imovel>> getTodos(){
+        return new ResponseEntity<>(imovelService.getTodos(), HttpStatus.OK);
+    }
+}
